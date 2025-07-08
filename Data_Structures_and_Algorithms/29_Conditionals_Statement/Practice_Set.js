bdb033c956c09f930351bf570a722f2fec16e84d
Diku Mardi <dikumardi047@gmail.com>
2025-07-08 23:12:53 +0530

    added js practice question

---
+++ Data_Structures_and_Algorithms/29_Conditionals_Statement/Practice_Set.js
@@ -0,0 +1,53 @@
+/*1. 1.	Declare a variable for your name */
+let name = "name"
+console.log(name);
+
+/**2.	Declare a constant for Pi (3.14). */
+const pi = 3.14 ;
+console.log(pi);
+
+/**3.	Check the type of 42, 'hello', true, null, undefined, NaN. */
+console.log(typeof(42));
+console.log(typeof('hello'));
+console.log(typeof(true));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(NaN));
+
+/**4.	Convert a string to number and vice versa. */
+let num = Number('12');
+console.log(num);
+
+let str = String(22)
+console.log(str);
+
+/**5.	Reassign a variable with new value. */
+let reasin = 12;
+reasin = 25;
+console.log(reasin);
+
+
+/** 6.	What happens if you reassign a const variable?*/
+// const ra = 2 ;
+// ra = 54 ;
+// console.log(ra);
+//Output---  Assignment to constant variable.
+
+/**7.	Add two numbers and print result. */
+
+let num1 = 10 ;
+let num2 = 20 ;
+sum = num1 + num2 ;
+console.log(sum);
+// 30
+
+/*8.	Subtract, multiply, divide two numbers. */
+sub = num1 - num2 ;
+console.log(sub);
+
+mul = num1 * num2 ;
+console.log(mul);
+
+div = num1 / num2 ;
+console.log(div);
+

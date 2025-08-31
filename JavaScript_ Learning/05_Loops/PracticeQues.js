af41d639e56e881b4f3f96b5acbb6720f11f4a24
Diku Mardi <dikumardi047@gmail.com>
2025-08-31 19:46:00 +0530

    added loop practice question

---
+++ JavaScript_ Learning/05_Loops/PracticeQues.js
@@ -0,0 +1,33 @@
+/** Practice Question **/
+//  1. Print 1 to 10 using for
+/**
+  for (let i = 1; i <= 10; i++) {
+   console.log(i);
+}
+    **/
+
+// 2. Print even numbers between 1 to 20 
+/** for (let i = 0; i <= 20; i++) {
+    if (i % 2 === 0) {
+    console.log("Even Number are : "+ i);
+    }    
+} **/
+
+//  3. Reverse a string using loop
+// let str  ="hello" ;
+// for (let i=str.length - 1  ; i >=0; i--) {
+//     console.log(str.length);
+    
+// }
+
+
+// 7. Use continue to skip a specific number
+// for (let i = 1; i <= 20; i++){
+//   if ( i === 7) {
+//     continue ; 
+//   }
+//       console.log(i);
+
+// }
+
+    

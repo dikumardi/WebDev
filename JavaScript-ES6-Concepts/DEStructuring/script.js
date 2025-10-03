3479289d38e6eefb151509f233e99473863933bc
Diku Mardi <dikumardi047@gmail.com>
2025-10-03 22:31:58 +0530

    Destructuring array and objects

---
+++ JavaScript-ES6-Concepts/DEStructuring/script.js
@@ -0,0 +1,24 @@
+ // Destructuring
+//Array Destructuring
+let list = ['Audi','BMW','Honda']
+
+let [car1,,car2] = list
+
+console.log(car1);
+console.log(car2);
+
+//Object Destructuring
+
+const person = {
+    name:"diku",
+    age:21,
+    
+}
+let {name,age} = person
+
+console.log(name);
+console.log(age);
+
+// console.log(person.age);
+
+

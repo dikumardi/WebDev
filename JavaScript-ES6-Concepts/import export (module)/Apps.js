cdd0757bd7252c36964a08b55f57f46c0ed28ba5
Diku Mardi <dikumardi047@gmail.com>
2025-10-03 22:32:36 +0530

    import export module in js

---
+++ JavaScript-ES6-Concepts/import export (module)/Apps.js
@@ -0,0 +1,11 @@
+import person from './customer.js';
+// import { hello, data as d } from './sell.js';
+import * as bundle  from './sell.js';
+
+console.log(person);
+
+// hello();
+// console.log(bundle);
+
+bundle.hello();
+console.log(bundle.data);

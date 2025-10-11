39283901576f4cab8f60e8150d9c178e2852358d
Diku Mardi <dikumardi047@gmail.com>
2025-10-11 20:29:28 +0530

    two way binding form element

--- React/intro/src/App.jsx
+++ React/intro/src/App.jsx
@@ -1,28 +1,59 @@
+import { useState } from "react";
 
 const App = () => {
+  const [todos, settodos] = useState([
+    { id: 1, title: "Work nn", isCompleted: false },
+  ]);
 
-   // js logic
-   // Non-parameterized function
-   const handleClick = ()=> {
-      alert("button Clicked")
-   }
-
-      // parameterized function
-      const handleparaClick = (msg)=>{
-        alert(msg);
-      }
-      
-//  const wrapperhandler = ()=> handleparaClick("Hello from parameter!")
-
+  const [title, settitle] = useState("")
+  const [completed, setcompleted] = useState(true)
+  const [gender, setgender] = useState("male")
+  const [city, setcity] = useState("mumbai")
 
   return (
-    <>
-    <h1>{2+3}</h1>
-    <div>App </div>
-    <button onClick={handleClick}> click </button>
-    <button onClick={()=> handleparaClick("Hello from parameter!") }> click This is parameter </button>
-    </>
-  )
-}
+    <div>
+      <h1>Create Task</h1>
+      <form>
+        <input
+          onChange={(e) => settitle(e.target.value)}
+          value={title}
+          type="text"
+          placeholder="Title"
+        />
+        <br /> <br />
 
-export default App
+        <input
+          checked={completed}
+        onChange={(e) => setcompleted(e.target.checked)}
+        type="checkbox" /> Completed
+        <br /> <br />
+       
+        <input     
+         onChange={(e) => setgender(e.target.value)}
+         value="male"
+         checked={gender == "male" && true}
+        type="radio"
+        /> male
+
+         <input     
+         onChange={(e) =>setgender(e.target.value)}
+         value="Female"
+         checked={gender == "female" && true}
+        type="radio"
+        /> Female
+        <br /> <br />
+        <select onChange={(e)=>setcity(e.target.value)}
+        value={city}
+        >
+        <option value="delhi">Delhi</option>
+        <option value="jaipur">jaipur</option>
+        <option value="mumbai">mumbai</option>
+        </select>
+
+        <button>Create Todo</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;

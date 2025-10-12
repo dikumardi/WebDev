7dbbfd67de4a1976c59aadf6070d10a38bcea94d
Diku Mardi <dikumardi047@gmail.com>
2025-10-12 22:46:10 +0530

    completed todo

--- React/intro/src/components/Read.jsx
+++ React/intro/src/components/Read.jsx
@@ -2,12 +2,26 @@ import { Fragment } from "react";
 
 const Read = (props) => {
     const todos =props.todos;
-    const settodos = todos.settodos
+    const settodos = props.settodos
 
+  const deletHandler = (id) =>{
+   console.log(id);
+  const filtersTodos = todos.filter((todo)=>todo.id !=id)
+  settodos(filtersTodos);
+ 
+  }
     
  const RenderView =todos.map(todo=>{
-  return <li style={{color: todo.isCompleted ? "green" : "red"}} key={todo.id}>{todo.title}</li>
- });
+  return (
+    <li 
+    style={{color: todo.isCompleted ? "green" : "red"}}
+     key={todo.id}>{todo.title} |{" "}
+      <span 
+      onClick={()=>deletHandler(todo.id)}
+      style={{color:"slateblue"}}>Delete</span>
+      </li>
+  )
+  });
 
   return (
     <Fragment>

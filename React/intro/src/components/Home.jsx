2a14ce3f42d1a4ced41a19fed690d745aa2abdc8
Diku Mardi <dikumardi047@gmail.com>
2025-10-20 23:26:38 +0530

    Learned API fetching using Axios and Fetch with async/await in React

--- React/intro/src/components/Home.jsx
+++ React/intro/src/components/Home.jsx
@@ -1,8 +1,28 @@
+import axios from 'axios';
 import React from 'react'
 
 const Home = () => {
+
+  const getproduct = async ()=>{
+    try {
+      //
+      // const response =await axios.get("https://fakestoreapi.com/products")
+      // console.log(response);
+      
+      const strdata = fetch("https://fakestoreapi.com/products")
+      const jsondata = (await strdata).json();
+      console.log(jsondata);
+      
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
   return (
-    <div>Home</div>
+    <div>
+    <h1>Home</h1>
+    <button onClick={getproduct}>Get Products</button>
+    </div>
   )
 }
 

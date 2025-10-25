245a61626f93e9e9218f983d3f9046f25c850c60
Diku Mardi <dikumardi047@gmail.com>
2025-10-25 22:59:44 +0530

    Initial project setup with folder structure

---
+++ React/Project/Frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import axios from './api/axiosconfig';
+
+
+const App = () => {
+  const getproduct = async () => {
+    try {
+      const res =await axios.get('/products');
+      console.log(res);
+      
+    } catch (error) {
+       console.log(error);
+       
+    }
+  }
+
+  useEffect(()=>{
+   getproduct();
+  },[])
+  return (
+    <div className='bg-red-500'>App</div>
+  )
+}
+
+export default App

1963b0f3481b5d1b611eba6936c7831027a7f3d0
Diku Mardi <dikumardi047@gmail.com>
2026-01-29 22:04:45 +0530

    updated day 6

--- Javascript_Exercise/Basics-Question/Day6.js
+++ Javascript_Exercise/Basics-Question/Day6.js
@@ -97,3 +97,91 @@ console.log(isPrime);
 
 
 
+
+
+// let str = 'hello';
+// let rev = '';
+
+// for (let i = str.length - 1; i >= 0 ; i--) {
+//    rev = rev + str[i];
+// }
+// console.log(rev);
+
+
+// let number = 121 ;
+// let isPalindrome = true;
+
+// for (let i = 0; i < number.length/2; i++) {
+    
+//     if (number[i] !== number[number.length-1 - i ])  {
+//         isPalindrome = false;
+//         break;
+//     }
+// }
+// console.log(isPalindrome);
+
+
+
+// let str = 'hello';
+// let 
+// h=0, 
+// e = 0 , 
+// l = 0 ,
+// o = 0 ;
+
+// for (let i = 0; i < str.length; i++) {
+//    if (str[i] === 'h') h++ ; 
+//   else if (str[i] === 'e') e++ ; 
+//   else if (str[i] === 'l') l++ ; 
+//   else if (str[i] === 'o') o++ ; 
+    
+// }
+// console.log('h', h);
+// console.log('e', e);
+// console.log('l', l);
+// console.log('o', o);
+
+
+/** 
+ * factorial of a number
+ let n = 5 ;
+let result = 1;
+for (let i = 1; i <= n; i++) {
+    result =result * i;
+    
+}
+console.log(result);
+
+
+   **/
+
+/**Prime or not 
+let n = 1 ;
+let isPrime = true ;
+if (n < 2){
+    isPrime = false
+}else{
+    for (let i = 2; i < n; i++) {
+        if (n % 2 === 0) {
+          isPrime =false;
+        }
+    }
+}
+console.log(isPrime);
+ 
+**/
+
+/**Find Largest number
+ let a = 10 ;
+let b = 21 ;
+let c = 17;
+
+if (a >= b && a >= c) {
+console.log('A is greater',a);    
+} else if (b >=a && b >= c) {
+    console.log('B is greater',b);
+    
+}else{
+        console.log('c is greater',c);
+}
+  **/

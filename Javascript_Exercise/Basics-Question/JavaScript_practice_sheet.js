71c39ce23546541c94d42f7db0174a89920ae036
Diku Mardi <dikumardi047@gmail.com>
2026-01-30 22:48:57 +0530

    js practice sheet

---
+++ Javascript_Exercise/Basics-Question/JavaScript_practice_sheet.js
@@ -0,0 +1,67 @@
+                       /** JavaScript Beginner Practice **/
+
+
+// ============================================
+// 1️⃣ Print numbers from 1 to 10
+
+// ============================================
+// 2️⃣ Print all odd numbers from 1 to 20
+
+
+// ============================================
+// 3️⃣ Print all even numbers from 1 to 20
+
+
+// ============================================
+// 4️⃣ Sum of numbers from 1 to 50
+
+
+// ============================================
+// 5️⃣ Sum of all even numbers from 1 to 50
+
+// ============================================
+// 6️⃣ Count numbers divisible by 5 between 1 and 50
+
+// ============================================
+// 7️⃣ Largest of 3 numbers
+
+
+// ============================================
+// 8️⃣ Count vowels in a string
+
+
+// ============================================
+// 9️⃣ Reverse a string
+
+
+// ============================================
+// 🔟 Check if a string is palindrome
+
+
+
+                         /** JavaScript Intermediate Beginner Practice **/
+                         
+// ============================================
+// 1️⃣ Print all elements of an array
+let arr1 = [2, 4, 6, 8, 10];
+
+for (let i = 0; i < arr1.length; i++) {
+    console.log(arr1[i]);
+}
+
+
+// ============================================
+// 2️⃣ Sum of elements in an array
+let arr2 = [1, 2, 3, 4, 5];
+
+let sum = 0 ;
+for (let i = 0; i < arr2.length; i++) {
+    sum = sum + arr2[i];
+}
+console.log(sum);
+
+// ============================================
+// 3️⃣ Find the largest element in an array
+let arr3 = [12, 7, 25, 18, 10];
+
+

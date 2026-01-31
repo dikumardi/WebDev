bba8193dd51db93a259504a90e5a1c5a386d742b
Diku Mardi <dikumardi047@gmail.com>
2026-01-31 20:18:16 +0530

    Add JS practice question with solution

--- Javascript_Exercise/Basics-Question/JavaScript_practice_sheet.js
+++ Javascript_Exercise/Basics-Question/JavaScript_practice_sheet.js
@@ -63,5 +63,150 @@ console.log(sum);
 // ============================================
 // 3️⃣ Find the largest element in an array
 let arr3 = [12, 7, 25, 18, 10];
+let largest = arr3[0] ;
+for (let i = 1; i < arr3.length; i++) {
+    if (largest <= arr3[i]) {
+         largest = arr3[i]         
+    }
+ }
+         console.log(largest);
+
+// ============================================
+// 4️⃣ Find the smallest element in an array
+let arr4 = [12, 7, 25, 18, 10];
+let smallest = arr4[0];
+for (let i = 1; i < arr4.length; i++) {
+   if (arr4[i] < smallest ) {
+    smallest = arr4[i];
+   }
+    
+}
+console.log(smallest);
+
+// ============================================
+// 5️⃣ Reverse an array
+let arr5 = [1, 2, 3, 4, 5];
+let rev = [];
+for (let i = arr5.length - 1 ; i >= 0 ; i--) {
+      rev.push(arr5[i])
+}
+console.log(rev);
+
+
+// ============================================
+// 6️⃣ Count even and odd numbers in an array
+let arr6 = [1, 2, 3, 4, 5, 6, 7, 8];
+
+let countEven = 0 ;
+let countOdd = 0 ;
+
+for (let i = 0; i < arr6.length; i++) {
+    if ( arr6[i] % 2 === 0) {
+        countEven++;
+    }else {
+     countOdd++;
+    }
+}
+console.log('Even count', countEven , "odd count", countOdd);
+
+
+
+// ============================================
+// 7️⃣ Sum of all elements divisible by 3 in an array
+let arr7 = [3, 5, 6, 9, 10, 12];
+let sumDiv3 = 0 ;
+for (let i = 0; i < arr7.length; i++) {
+  if (arr7[i] % 3 === 0) {
+     sumDiv3 += arr7[i]
+  }    
+}
+console.log('Sum of element divisible by 3 is :',sumDiv3);
+
+
+
+// ============================================
+// 8️⃣ Reverse a string using array method
+let str1 = "javascript";
+let reverse = str1.split('').reverse().join('') 
+console.log(reverse);
+
+// ============================================
+// 9️⃣ Count vowels in a string using loop
+let str2 = "programming";
+let countVowels = 0;
+for (let i = 0; i < str2.length; i++) {
+    if ('aeiou'.includes(str2[i])) {
+        countVowels++;
+        // str2[i] === 'a' ||
+//         str2[i] === 'e' ||
+//         str2[i] === 'i' ||
+//         str2[i] === 'o' ||
+//         str2[i] === 'u'
+    }
+    
+}
+console.log(countVowels);
+
+
+
+// ============================================
+// 🔟 Check if a string is a palindrome (array method)
+let str3 = "madam";
+let revStr = str3.split('').reverse().join('');
+let isPalindrome = (str3 === revStr)
+console.log(isPalindrome);
+
+
+// ============================================
+// 1️⃣1️⃣ Find duplicate elements in an array
+let arr8 = [1, 2, 3, 2, 4, 5, 3];
+let duplicate = [] ;
+for (let i = 0; i < arr8.length; i++) {
+    
+    for(j = i + 1 ; i < arr8.length; i++){
+        if (arr8[i] === arr8[j] && !duplicate.includes(arr8[i])) {
+            duplicate.push(arr8[i])
+        }
+    }
+    
+}
+console.log('Duplicate :', duplicate);
+
+
+
+
+// ============================================
+// 1️⃣2️⃣ Merge two arrays
+let arr9 = [1, 2, 3];
+let arr10 = [4, 5, 6];
+let mergeArrays = arr9.concat(arr10);
+console.log(mergeArrays);
+
+// ============================================
+// 1️⃣3️⃣ Find sum of numbers in mixed array (numbers + strings)
+let arr11 = [1, "a", 2, "b", 3];
+let sumNumber = 0 ;
+for (let i = 0; i < arr11.length; i++) {
+    if (typeof arr11[i] === 'number') {
+        sumNumber += arr11[i]
+    }
+}
+
+console.log(sumNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

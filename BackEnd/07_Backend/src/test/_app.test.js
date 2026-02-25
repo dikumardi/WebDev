939383d43d73efd56d6e310f5d681df663f0a25c
Diku Mardi <dikumardi047@gmail.com>
2026-02-25 22:40:17 +0530

    Testing with jest and supertest

---
+++ BackEnd/07_Backend/src/test/_app.test.js
@@ -0,0 +1,15 @@
+const request  = require("supertest");
+
+const app = require("../app")
+
+describe("GET /", () => { 
+    it("should return 200 Ok", async()=> {
+
+        const res = await request(app).get("/");
+
+        // expect(res.statusCode).toBe(200);
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({message:"Hello world"})
+        
+    })
+ })

fb3efe9a0b300b6a249d210042f039a3a868d880
Diku Mardi <dikumardi047@gmail.com>
2026-03-05 23:33:06 +0530

    Add authentication routes with JWT and cookies

--- BackEnd/Backend-Learning/Day_9_Auth/src/routes/auth.routes.js
+++ BackEnd/Backend-Learning/Day_9_Auth/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userModel = require('../model/user.model');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router()
 
@@ -17,9 +18,16 @@ router.post('/register',async(req,res) => {
     const user = await userModel.create({
       username,password
     })
+    const token = jwt.sign({
+        id:user._id,
+    },process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+    
     res.status(201).json({
         message:"User registered successfully",
-        user
+        user,
+        
     })
 })
 
@@ -47,6 +55,32 @@ router.post('/login', async(req,res) => {
         message:"User LoggedIn successfully"
       })
 
+})
+
+router.get('/user', async(req,res) => {
+    const {token} = req.cookies
+    if (!token) {
+        return res.status(401).json(
+        {message:"Unauthorized"})
+    }
+  
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+
+           const user = await userModel.findOne({
+            _id:decoded.id
+           }).select("-password -__v")
+        res.status(200).json({
+            message:"User Data Fetched Successfully",
+            user
+        })
+
+    } catch (err) {
+         return res.status(401).json({
+            message:"Unauthorized -Invalid Token"
+         })
+    }
+    
 })
 
 
